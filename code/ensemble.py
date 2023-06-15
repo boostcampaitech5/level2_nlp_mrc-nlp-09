d6f28f8f89b1d361d117ecc5e33c7fd65a8cd1a1
Junsun Lee <junsun1014@naver.com>
2023-06-15 04:55:22 +0000

    feat: add weighted vote
    #27

--- code/ensemble.py
+++ code/ensemble.py
@@ -32,6 +32,7 @@ def hard_voting():
             else:
                 temp[result[i][j]] = 1
         final_result[keys[i]] = max(temp, key=temp.get)
+
         max_count = max(temp.values())
         for key, value in temp.items():
             if value == max_count and key != final_result[keys[i]]:
@@ -41,6 +42,7 @@ def hard_voting():
                 print(f"동률 단어: {key}")
                 print()
                 count += 1
+
     print(f"동률 개수: {count}")
 
     # 결과 저장
@@ -48,7 +50,55 @@ def hard_voting():
         json.dump(final_result, f, indent=4, ensure_ascii=False)
 
 
-def soft_voting():
+def hard_voting_with_weight():
+    path = "../ensemble/hard"
+    ensemble_lists = os.listdir(path)
+
+    # 예측값들을 리스트에 저장
+    answers = []
+    for ensemble_list in ensemble_lists:
+        with open(os.path.join(path, ensemble_list), "r") as f:
+            answers.append(json.load(f))
+
+    # 각 데이터별 예측값들을 리스트에 저장
+    keys = list(answers[0].keys())
+    result = []
+    for i in range(len(answers[0])):
+        temp = []
+        for j in range(len(answers)):
+            temp.append(answers[j][keys[i]])
+        result.append(temp)
+
+    # 빈도수 기반 투표
+    final_result = {}
+    count = 0
+    for i in range(len(result)):
+        temp = {}
+        for j in range(len(result[i])):
+            if result[i][j] in temp:
+                temp[result[i][j]] += float(ensemble_lists[j][-8:-5])*0.001
+            else:
+                temp[result[i][j]] = float(ensemble_lists[j][-8:-5])*0.001
+        final_result[keys[i]] = max(temp, key=temp.get)
+
+        max_count = max(temp.values())
+        for key, value in temp.items():
+            if value == max_count and key != final_result[keys[i]]:
+                print("동률 발생:", keys[i])
+                print(temp)
+                print(f"선택된 정담: {final_result[keys[i]]}")
+                print(f"동률 단어: {key}")
+                print()
+                count += 1
+
+    print(f"동률 개수: {count}")
+
+    # 결과 저장
+    with open("../ensemble/ensemble_hard_weight.json", "w") as f:
+        json.dump(final_result, f, indent=4, ensure_ascii=False)
+
+
+def soft_voting(weighted):
     path = "../ensemble/soft"
     ensemble_lists = os.listdir(path)
 
@@ -69,16 +119,27 @@ def soft_voting():
                 found = False
                 for index, value in enumerate(temp):
                     if value[0] == answers[j][keys[i]][k]["text"]:
-                        temp[index][1] += answers[j][keys[i]][k]["probability"]
+                        if weighted:
+                            temp[index][1] += answers[j][keys[i]][k]["probability"] * \
+                                (float(ensemble_lists[j][-8:-5])*0.001)
+                        else:
+                            temp[index][1] += answers[j][keys[i]
+                                                         ][k]["probability"]
                         found = True
                         break
                 if not found:
-                    temp.append(
-                        [answers[j][keys[i]][k]["text"],
-                         answers[j][keys[i]][k]["probability"]]
-                    )
-        result.append(temp)
+                    if weighted:
+                        temp.append(
+                            [answers[j][keys[i]][k]["text"],
+                             answers[j][keys[i]][k]["probability"] * (float(ensemble_lists[j][-8:-5])*0.001)]
+                        )
+                    else:
+                        temp.append(
+                            [answers[j][keys[i]][k]["text"],
+                             answers[j][keys[i]][k]["probability"]]
+                        )
         temp.sort(key=lambda x: x[1], reverse=True)
+        result.append(temp)
 
     # 확률 기반 투표
     final_result = {}
@@ -86,10 +147,11 @@ def soft_voting():
         final_result[keys[i]] = result[i][0][0]
 
     # 결과 저장
-    with open("../ensemble/ensemble_soft.json", "w") as f:
+    with open("../ensemble/ensemble_soft_weight.json", "w") as f:
         json.dump(final_result, f, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
-    hard_voting()
-    # soft_voting()
+    # hard_voting()
+    hard_voting_with_weight()
+    # soft_voting(weighted=True)
