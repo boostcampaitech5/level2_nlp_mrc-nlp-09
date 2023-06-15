63d5bcce659a1da3eb7bbdf61f1267cebdfa5dd1
TrainToGPB <kaiba0514@gmail.com>
2023-06-15 13:36:46 +0000

    feat: add data cleaning and warmup steps
    #29

--- pretraining/wiki_pretrain.py
+++ pretraining/wiki_pretrain.py
@@ -1,16 +1,19 @@
 import torch
 import json
 import wandb
+import re
 from tqdm import tqdm
 from transformers import AutoTokenizer, RobertaForMaskedLM
 from transformers import TextDataset, LineByLineTextDataset, DataCollatorForLanguageModeling
+from transformers import EvalPrediction
 from transformers import Trainer, TrainingArguments
 from transformers import set_seed
+from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import train_test_split
 
 
 def main():
-    wandb.init(project="klue_mrc_pretrain", name="wiki_pretrain")
+    wandb.init(project="klue_mrc_pretrain", name="wiki_pretrain") # klue_mrc_pretrain
     
     set_seed(42)
 
@@ -22,16 +25,30 @@ def main():
     def json_to_datasets(data_path, output_path, train_path, eval_path):
         with open(data_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
-        print("[Extracting Texts from Documents]")
+        print("[Extracting Texts from JSON Documents]")
         texts = [data[key]['text'] for key in tqdm(data)]
         print("[Extracting Lines from Texts]")
-        lines = [line for text in tqdm(texts) for line in text.split('\n') if line.strip()]
+        lines = []
+        for text in tqdm(texts):
+            for line in text.split('\n'):
+                if line.strip() and len(line.strip()) > 50:
+                    if re.search('[가-힣]', line):
+                        if line.startswith(": "):
+                            line = line[2:]
+                        if '|' in line:
+                            continue
+
+                        line = re.sub(r'\n', ' ', line)
+                        line = re.sub(r' {2,}', ' ', line)
+
+                        if re.search('^[가-힣A-Za-z0-9一-龥]', line):
+                            lines.append(line.strip())
 
         # lines를 텍스트 파일로 저장
         with open(output_path, 'w', encoding='utf-8') as file:
             file.write('\n'.join(lines))
 
-        train_lines, eval_lines = train_test_split(lines, test_size=0.2, random_state=42)
+        train_lines, eval_lines = train_test_split(lines, test_size=0.1, random_state=42)
         with open(train_path, 'w', encoding='utf-8') as file:
             file.write('\n'.join(train_lines))
         with open(eval_path, 'w', encoding='utf-8') as file:
@@ -64,16 +81,37 @@ def main():
         per_device_train_batch_size=16,  # 배치 크기
         per_device_eval_batch_size=16,  # 배치 크기
         save_total_limit=2,  # 저장할 체크포인트의 최대 개수
-        prediction_loss_only=True,  # MLM 손실 함수만 사용
+        prediction_loss_only=False,  # MLM 손실 함수만 사용
         learning_rate=1e-4,  # 학습률
+        logging_strategy='steps',
         logging_steps=100,
         save_steps=5000,
         eval_steps=5000,
         save_strategy="steps",
         evaluation_strategy="steps",
         load_best_model_at_end=True,
-        metric_for_best_model="loss",
+        metric_for_best_model="accuracy",
+        report_to="wandb",
+        greater_is_better=True,
+        seed=42,
+        warmup_steps=5000,
     )
+
+    wandb.config.update(training_args)
+
+    def compute_metrics(pred):
+        labels = pred.label_ids
+        preds = torch.argmax(torch.tensor(pred.predictions), dim=-1).cpu().numpy()
+        # print(type(labels))
+        # print(labels.shape)
+        # print(type(preds))
+        # print(preds.shape)
+        acc = accuracy_score(labels.flatten(), preds.flatten())
+        f1 = f1_score(labels.flatten(), preds.flatten(), average='weighted')
+        return {
+            'accuracy': acc,
+            'f1': f1,
+        }
 
     # Trainer 객체 생성 및 훈련 실행
     trainer = Trainer(
@@ -82,13 +120,10 @@ def main():
         data_collator=data_collator,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
+        compute_metrics=compute_metrics,
     )
 
     trainer.train()
-
-    eval_result = trainer.evaluate()
-    perplexity = torch.exp(eval_result["eval_loss"])
-    print(f"Perplexity: {perplexity.item()}")
 
 if __name__ == "__main__":
     main()
